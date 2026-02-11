16f35b4227326437ab28b1cba71fc7e1f98a3704
karthickr6363-hub <karthickr6363@gmail.com>
2026-02-11 22:50:05 +0530

    add the some changes

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@
         }, 1);
     };
     spinner(0);
-    
-    
+
+
     // Initiate the wowjs
     new WOW().init();
 
@@ -24,18 +24,18 @@
             $('.sticky-top').removeClass('shadow-sm').css('top', '-200px');
         }
     });
-    
-    
-   // Back to top button
-   $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-        $('.back-to-top').fadeIn('slow');
-    } else {
-        $('.back-to-top').fadeOut('slow');
-    }
+
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
 
@@ -48,27 +48,27 @@
         dots: true,
         loop: true,
         margin: 25,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="bi bi-arrow-left"></i>',
             '<i class="bi bi-arrow-right"></i>'
         ],
         responsiveClass: true,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            576:{
-                items:2
+            576: {
+                items: 2
             },
-            768:{
-                items:2
+            768: {
+                items: 2
             },
-            992:{
-                items:3
+            992: {
+                items: 3
             },
-            1200:{
-                items:4
+            1200: {
+                items: 4
             }
         }
     });
@@ -81,27 +81,27 @@
         dots: true,
         loop: true,
         margin: 25,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="bi bi-arrow-left"></i>',
             '<i class="bi bi-arrow-right"></i>'
         ],
         responsiveClass: true,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            576:{
-                items:1
+            576: {
+                items: 1
             },
-            768:{
-                items:1
+            768: {
+                items: 1
             },
-            992:{
-                items:2
+            992: {
+                items: 2
             },
-            1200:{
-                items:2
+            1200: {
+                items: 2
             }
         }
     });
@@ -143,6 +143,25 @@
         }
     });
 
+    // Dynamic Active Nav Link Highlighting
+    $(document).ready(function () {
+        var path = window.location.pathname.split("/").pop();
+        if (path == '') {
+            path = 'index.html';
+        }
+
+        $('.navbar-nav .nav-link, .navbar-nav .dropdown-item, .sidebar .nav-link').each(function () {
+            var href = $(this).attr('href');
+            if (path === href) {
+                $(this).addClass('active');
+                if ($(this).hasClass('dropdown-item')) {
+                    $(this).closest('.nav-item.dropdown').find('.nav-link.dropdown-toggle').addClass('active');
+                }
+            } else {
+                $(this).removeClass('active');
+            }
+        });
+    });
 
 })(jQuery);
 
